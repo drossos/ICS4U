cec301dc5abeb273a13c912c94d3a59f4687146f
drossos <drossos@bayviewglen.ca>
2017-09-29 19:25:41 -0400

    Tree and nodes not being written

---
+++ Unit One  - Data Structures and Collections/src/com/bayviewglen/tree/BinarySearchTree.java
@@ -0,0 +1,73 @@
+package com.bayviewglen.tree;
+
+public class BinarySearchTree {
+	private IntTreeNode root;
+
+	public BinarySearchTree(IntTreeNode root) {
+		super();
+		this.root = root;
+	}
+
+	public BinarySearchTree() {
+		super();
+		this.root = null;
+	}
+
+	public void inorderTraversal(IntTreeNode current) {
+		if (current.getLeft() != null)
+			inorderTraversal(current.getLeft());
+		evaluate(current);
+		if (current.getRight() != null)
+			inorderTraversal(current.getRight());
+	}
+
+	public void preorderTraversal(IntTreeNode current) {
+		evaluate(current);
+
+		if (current.getLeft() != null)
+			inorderTraversal(current.getLeft());
+		if (current.getRight() != null)
+			inorderTraversal(current.getRight());
+	}
+
+	public void postorderTraversal(IntTreeNode current) {
+		if (current.getLeft() != null)
+			inorderTraversal(current.getLeft());
+		if (current.getRight() != null)
+			inorderTraversal(current.getRight());
+		evaluate(current);
+	}
+
+	public void add(IntTreeNode current, int x) {
+		IntTreeNode temp = new IntTreeNode(x);
+
+		if (current == null) {
+			current = temp;
+		} else if (x < current.getData()) {
+			add(current.getLeft(), x);
+		} else if (x >= current.getData()) {
+			add(current.getRight(), x);
+		}
+	}
+
+	public void add(int x) {
+		if (root == null) {
+			IntTreeNode temp = new IntTreeNode(x);
+			root = temp;
+		} else
+			add(root, x);
+	}
+
+	private void evaluate(IntTreeNode current) {
+		System.out.println(current.getData());
+	}
+
+	public IntTreeNode getRoot() {
+		return root;
+	}
+
+	public void setRoot(IntTreeNode root) {
+		this.root = root;
+	}
+
+}
---
+++ Unit One  - Data Structures and Collections/src/com/bayviewglen/tree/TreeDriver.java
@@ -0,0 +1,8 @@
+package com.bayviewglen.tree;
+
+public class TreeDriver {
+
+		public static void main (String [] args) {
+			
+		}
+}

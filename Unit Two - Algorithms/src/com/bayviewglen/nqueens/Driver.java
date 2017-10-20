1d953196237f12fd295b2f8d40d7c92cf3cc2b1a
drossos <drossos@bayviewglen.ca>
2017-10-19 22:47:43 -0400

    nqueens working

--- Unit Two - Algorithms/src/com/bayviewglen/nqueens/Driver.java
+++ Unit Two - Algorithms/src/com/bayviewglen/nqueens/Driver.java
@@ -9,11 +9,11 @@ public class Driver {
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
 		int n = Integer.parseInt(read.nextLine());
-
 		solveQueens(n);
 	}
 
 	private static void solveQueens(int n) {
+		boolean validSln = true;
 		n -= 1;
 		int filled = 1;
 		Stack<Queen> stack = new Stack<Queen>();
@@ -21,37 +21,52 @@ public class Driver {
 		int row = 1;
 		int col = 0;
 		while (filled <= n) {
-			if (!conflict(col, row, stack) && row <= n) {
+			if (stack.size() == 0 && col > n) {
+				System.out.println("No Soloution");
+				validSln = false;
+				break;
+			}
+			if (!conflict(col, row, stack) && col <= n) {
 				stack.push(new Queen(col, row));
 				filled++;
 				row++;
 				col = 0;
-			} else if (conflict(col, row, stack) && row <= n) {
+			} else if (conflict(col, row, stack) && col <= n) {
 				col++;
-				
-			} else if (stack.size() == 1 && stack.peek().getX() == n) {
+
+		/*	} else if (stack.size() == 1 && stack.peek().getX() == n) {
 				System.out.println("No Soloution");
-				break;
+				validSln = false;
+				break;*/
 			}
-			
+			/*if(col > n && stack.size() == 1) {
+				System.out.println("No Soloution");
+				validSln = false;
+				break;
+			}*/
 			if (col > n) {
 				filled--;
-				stack.pop();
 				col = stack.peek().getX() + 1;
+				stack.pop();
 				row--;
 			}
 
 		}
-
-		for (int i = 0; i <= n; i++) {
-			for (int k = 0; k <= n; k++) {
-				if (stack.peek().getX() == k) {
-					stack.pop();
-					System.out.print("Q ");
-				} else
-					System.out.print(". ");
+		if (validSln) {
+			int temp = stack.size();
+			boolean foundForRow = false;
+			for (int i = 0; i < temp; i++) {
+				for (int k = 0; k < temp; k++) {
+					if (!foundForRow && stack.peek().getX() == k) {
+						stack.pop();
+						System.out.print("Q ");
+						foundForRow = true;
+					} else
+						System.out.print(". ");
+				}
+				foundForRow = false;
+				System.out.println();
 			}
-			System.out.println();
 		}
 
 	}
@@ -59,11 +74,12 @@ public class Driver {
 	private static boolean conflict(int col, int row, Stack<Queen> stack) {
 		Iterator<Queen> iter = stack.iterator();
 
-	for (int i = stack.size()-1; i >= 0; i --) {
+		for (int i = stack.size() - 1; i >= 0; i--) {
 
 			if (stack.get(i).getX() == col)
 				return true;
-			else if (Math.abs((row - stack.get(i).getY()) / (col - stack.get(i).getX())) == 1)
+			/*else if (Math.abs((row - stack.get(i).getY()) / (col - stack.get(i).getX())) == 1)*/
+			else if (Math.abs(row-stack.get(i).getY()) == Math.abs(col - stack.get(i).getX()))
 				return true;
 		}
 		return false;

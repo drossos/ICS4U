140d88cf4efd3d8af2df4ede47eb9805706f6b96
drossos <drossos@bayviewglen.ca>
2017-11-01 21:15:30 -0400

    zig zag not working large arrays

--- Unit Two - Algorithms/src/com/bayviewglen/dpproblemset/ZigZag.java
+++ Unit Two - Algorithms/src/com/bayviewglen/dpproblemset/ZigZag.java
@@ -3,42 +3,36 @@ package com.bayviewglen.dpproblemset;
 public class ZigZag {
 
 	public static void main(String[] args) {
-		int [] nums = {4,3,2,1};
-		int [] sln = new int[nums.length+1];
-		sln[0] = 0;
-		sln[1] = 1;
-		boolean increasing = true;
-		boolean either = true;
-		if (nums.length > 1) {
-		if (nums[1] > nums[0])
-			increasing = true;
-		else if (nums[1] < nums[0])
-			increasing = false;
-		else 
-			either = true;
-		
-		}
-		int toBeComapred = nums[0];
-		for (int i = 2; i < sln.length; i++) {
-			if (nums[i-1] > toBeComapred && increasing == true || either && nums[i-1] != toBeComapred) {
-				sln[i] = sln[i-1] + 1;
-				increasing = false;
-				either = false;
-				toBeComapred = nums[i-1];
-			} else if (nums[i-1] < nums[i-2] && increasing == false || either && nums[i-1] != toBeComapred) {
-				sln[i] = sln[i-1] + 1;
-				increasing = true;
-				either = false;
-				toBeComapred = nums[i-1];
+		int[] nums = {8 ,1, 7, 2, 6, 3, 5, 4};
+		int[] slnIncrs = new int[nums.length + 1];
+		int[] slnDecrs = new int[nums.length + 1];
+		//two cases, one where final nums are decreasing and one where final nums 
+		//are increasing, have to decide which of those is better option
+		slnIncrs[0] = 0;
+		slnIncrs[1] = 1;
+		slnDecrs[0] = 0;
+		slnDecrs[1] = 1;
+
+		for (int i = 2; i < slnIncrs.length; i++) {
+			//if the difference is possitve, final nums increasing
+			if (nums[i-1] - nums[i - 2] > 0) {
+				//this sln is either one more than the derc sln, since after going down go up so length up by one
+
+				slnIncrs[i] = (slnDecrs[i - 1] + 1);
+				slnDecrs[i] = slnDecrs[i - 1];
+
+			} else if (nums[i-1] - nums[i - 2] < 0) {
+				slnDecrs[i] = (slnIncrs[i - 1] + 1);
+				slnIncrs[i] = slnIncrs[i - 1];
+
 			} else {
-				sln[i] = sln[i-1];
+				slnIncrs[i] = slnIncrs[i-1];
+				slnDecrs[i] = slnDecrs[i-1];
 			}
 		}
-		
-		System.out.println(""+sln[sln.length-1]);
+
+		 System.out.println( Math.max(slnIncrs[nums.length], slnDecrs[nums.length]));   
 
 	}
-	
-	
 
 }

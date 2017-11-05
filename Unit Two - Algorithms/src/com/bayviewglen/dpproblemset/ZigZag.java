660916e5eef1a9d06fe862e2833fa1e5200bba6b
drossos <drossos@bayviewglen.ca>
2017-11-05 18:15:08 -0500

    zig zag with minor logic fix so works on all casese

--- Unit Two - Algorithms/src/com/bayviewglen/dpproblemset/ZigZag.java
+++ Unit Two - Algorithms/src/com/bayviewglen/dpproblemset/ZigZag.java
@@ -1,40 +1,58 @@
 package com.bayviewglen.dpproblemset;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ZigZag {
 
-	public static void main(String[] args) {
-		int [] nums = {-5 ,4, -2, 4, -4, -1, 4, -1, 5, 1 };
-		
-		
-		int [] sln = new int[nums.length+1];
-		sln[0] = 0;
-		sln[1] = 1;
-		boolean increasing = true;
-		if (nums.length > 1) {
-		if (nums[1] > nums[0])
-			increasing = true;
-		else 
-			increasing = false;
-	
-		
-		}
-		
-		for (int i = 2; i < sln.length; i++) {
-			if (nums[i-1] > nums[i-2] && increasing == true && nums[i-1] != nums[i-2]) {
-				sln[i] = sln[i-1] + 1;
-				increasing = false;
-			} else if (nums[i-1] < nums[i-2] && increasing == false && nums[i-1] != nums[i-2]) {
-				sln[i] = sln[i-1] + 1;
-				increasing = true;
-			} else {
-				sln[i] = sln[i-1];
-			}
-		}
-		
-		System.out.println(""+sln[sln.length-1]);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader input = new BufferedReader(new FileReader("data/ZigZag.dat"));
+		int lim = Integer.parseInt(input.readLine());
+		//int[] nums = { 3,3,4 };
 
+		for (int j = 0; j < lim; j++) {
+			input.readLine();
+			String [] numsS = input.readLine().split(" ");
+			int[]nums = new int[numsS.length];
+			for (int k =0 ; k < numsS.length; k++) {
+				nums[k] = Integer.parseInt(numsS[k]);
+			}
+			int[] sln = new int[nums.length + 1];
+			sln[0] = 0;
+			sln[1] = 1;
+			boolean increasing = true;
+			boolean either = false;
+			if (nums.length > 1) {
+				if (nums[1] > nums[0])
+					increasing = true;
+				else if (nums[1] < nums[0])
+					increasing = false;
+				else 
+					either = true;
+
+			}
+
+			for (int i = 2; i < sln.length; i++) {
+				if (nums[i - 1] > nums[i - 2] && increasing  || either && nums[i - 1] != nums[i - 2]) {
+					sln[i] = sln[i - 1] + 1;
+					increasing = false;
+					if (either)
+						either = false;
+				} else if (nums[i - 1] < nums[i - 2] && !increasing || either && nums[i - 1] != nums[i - 2]) {
+					sln[i] = sln[i - 1] + 1;
+					increasing = true;
+					if (either)
+						either = false;
+				} else {
+					sln[i] = sln[i - 1];
+				}
+			}
+
+			System.out.println("" + sln[sln.length - 1]);
+
+		}
 	}
-	
-	
 
 }

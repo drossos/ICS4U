a09bd2ace61b3d2fde149bc858b3975ca6e3436b
drossos <drossos@bayviewglen.ca>
2017-11-05 21:14:53 -0500

    test cases working

--- Unit Two - Algorithms/src/com/bayviewglen/dpproblemset/ContiguousSeq.java
+++ Unit Two - Algorithms/src/com/bayviewglen/dpproblemset/ContiguousSeq.java
@@ -1,32 +1,46 @@
 package com.bayviewglen.dpproblemset;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ContiguousSeq {
 
-	public static void main(String[] args) {
-		int[] nums = {5,15,-30,10,-5,40,10};
-		int [] sln = new int[nums.length+1];
-		
-		sln[0] = 0;
-		sln[1] = nums[0];
-		
-		ArrayList<Integer> subArray = new ArrayList<Integer>();
-		subArray.add(nums[0]);
-		for (int i = 2; i < sln.length; i++) {
-			if (sln[i-1] + nums[i-1] > nums[i-1]) {
-				sln[i] = sln[i-1] + nums[i-1];
-				subArray.add(nums[i-1]);
-			} else if (sln[i-1] + nums[i-1] < nums[i-1]) {
-				sln[i] = nums[i-1];
-				subArray.clear();
-				subArray.add(nums[i-1]);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader input = new BufferedReader(new FileReader("data/Contig.dat"));
+		int lim = Integer.parseInt(input.readLine());
+		//int[] nums = { 0 ,-1, 2 };
+
+		for (int j = 0; j < lim; j++) {
+			input.readLine();
+			String[] srngNums = input.readLine().split(" ");
+			int[] nums = new int[srngNums.length];
+			for (int i = 0; i < srngNums.length; i++) {
+				nums[i] = Integer.parseInt(srngNums[i]);
 			}
-			
+			int[] sln = new int[nums.length + 1];
+
+			sln[0] = 0;
+			ArrayList<Integer> subArray = new ArrayList<Integer>();
+
+			sln[1] = nums[0];
+			subArray.add(nums[0]);
+
+			for (int i = 2; i < sln.length; i++) {
+				if (sln[i - 1] + nums[i - 1] >= nums[i - 1]) {
+					sln[i] = sln[i - 1] + nums[i - 1];
+					subArray.add(nums[i - 1]);
+				} else if (sln[i - 1] + nums[i - 1] < nums[i - 1]) {
+					sln[i] = nums[i - 1];
+					subArray.clear();
+					subArray.add(nums[i - 1]);
+				}
+
+			}
+			int sum = Math.max(0, sum(subArray));
+			System.out.println(sum);
 		}
-		
-		System.out.println(sum(subArray));
-		
 
 	}
 
@@ -37,7 +51,5 @@ public class ContiguousSeq {
 		}
 		return sum;
 	}
-	
-
 
 }

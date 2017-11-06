43b7f020d814d29233a08b6a4bf44827d9ec2680
drossos <drossos@bayviewglen.ca>
2017-11-05 22:20:55 -0500

    working contig

--- Unit Two - Algorithms/src/com/bayviewglen/dpproblemset/ContiguousSeq.java
+++ Unit Two - Algorithms/src/com/bayviewglen/dpproblemset/ContiguousSeq.java
@@ -10,7 +10,7 @@ public class ContiguousSeq {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader input = new BufferedReader(new FileReader("data/Contig.dat"));
 		int lim = Integer.parseInt(input.readLine());
-		//int[] nums = { 0 ,-1, 2 };
+		 //int[] nums = { 3,-3 };
 
 		for (int j = 0; j < lim; j++) {
 			input.readLine();
@@ -22,34 +22,33 @@ public class ContiguousSeq {
 			int[] sln = new int[nums.length + 1];
 
 			sln[0] = 0;
-			ArrayList<Integer> subArray = new ArrayList<Integer>();
+			int highestValue = 0;
 
-			sln[1] = nums[0];
-			subArray.add(nums[0]);
+			//sln[1] = nums[0];
+			/*if (nums[0] >= 0)
+				highestValue += nums[0];*/
 
-			for (int i = 2; i < sln.length; i++) {
+			for (int i = 1; i < sln.length; i++) {
 				if (sln[i - 1] + nums[i - 1] >= nums[i - 1]) {
 					sln[i] = sln[i - 1] + nums[i - 1];
-					subArray.add(nums[i - 1]);
 				} else if (sln[i - 1] + nums[i - 1] < nums[i - 1]) {
 					sln[i] = nums[i - 1];
-					subArray.clear();
-					subArray.add(nums[i - 1]);
+					
 				}
+				highestValue = Math.max(highestValue,sln[i]);
 
 			}
-			int sum = Math.max(0, sum(subArray));
-			System.out.println(sum);
+			System.out.println(highestValue);
 		}
 
 	}
 
-	private static int sum(ArrayList<Integer> subArray) {
+	/*private static int sum(ArrayList<Integer> subArray) {
 		int sum = 0;
 		for (int i = 0; i < subArray.size(); i++) {
 			sum += subArray.get(i);
 		}
 		return sum;
-	}
+	}*/
 
 }
